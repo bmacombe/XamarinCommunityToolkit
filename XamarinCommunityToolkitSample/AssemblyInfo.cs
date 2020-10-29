b76c425c02c5d119f6dc7b5f7f6537b1165d6ce3
Brian Macomber <brian@agspectrum.com>
2020-10-29 14:36:45 -0500

    add attribute

--- XamarinCommunityToolkitSample/AssemblyInfo.cs
+++ XamarinCommunityToolkitSample/AssemblyInfo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Resources;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 [assembly: NeutralResourcesLanguage("en")]
+
+[assembly: Preserve]
